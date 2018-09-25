2b565ecb058615c98ade61ac6af37de01a9ad1e7
SIR4GO <e.m.ahmed@stud.fci-cu.edu.eg>
2018-09-26 01:29:29 +0200

    angularx social last update

--- src/app/Routes/Routes.ts
+++ src/app/Routes/Routes.ts
@@ -1,10 +1,16 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from '../Components/home/home.component';
+import {NotFoundComponent} from '../Components/not-found/not-found.component';
+import {HomeSignupComponent} from '../Components/home-signup/home-signup.component';
+import {FollowerSignupComponent} from '../Components/home-signup/follower-signup/follower-signup.component';
+import {AuthorSignupComponent} from '../Components/home-signup/author-signup/author-signup.component';
 
 
 export const routes: Routes = [
-  {path: '' , component: HomeComponent},
-  {path: '*' , component: HomeComponent},
-
-
+  {path: '' , component: HomeComponent },
+  {path: 'signup' , component: HomeSignupComponent , children: [
+      {path: 'follower' , component: FollowerSignupComponent },
+      {path: 'author' , component: AuthorSignupComponent  }
+    ]},
+  {path: '**', redirectTo: '' },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
 import {FormsModule} from '@angular/forms';
@@ -8,20 +7,57 @@ import {RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import {routes} from './Routes/Routes';
 import { NotFoundComponent } from './Components/not-found/not-found.component';
+import {APP_BASE_HREF} from '@angular/common';
+import { HomeSignupComponent } from './Components/home-signup/home-signup.component';
+import { FollowerSignupComponent } from './Components/home-signup/follower-signup/follower-signup.component';
+import { AuthorSignupComponent } from './Components/home-signup/author-signup/author-signup.component';
+import { SocialLoginModule, AuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider, LinkedInLoginProvider} from 'angularx-social-login';
+
+
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("371111978583-c2sbhkag9ervm0qtv4j5ur3jbkltqi88.apps.googleusercontent.com")
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider("268815873963865")
+  },
+  {
+    id: LinkedInLoginProvider.PROVIDER_ID,
+    provider: new LinkedInLoginProvider("LinkedIn-client-Id", false, 'en_US')
+  }
+]);
+
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    HomeSignupComponent,
+    FollowerSignupComponent,
+    AuthorSignupComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(routes),
-    HttpClientModule
+    HttpClientModule,
+    SocialLoginModule,
   ],
-  providers: [],
+  providers: [
+    {
+    provide: AuthServiceConfig,
+    useFactory: provideConfig
+  },
+
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
